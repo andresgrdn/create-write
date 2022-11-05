cd0982ddccc8e5b456b07685ac238298f27c069e
Andres Gordon <gordon_andres@yahoo.es>
2022-11-05 18:17:11 -0500

    ✨ [ADD] Writing feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import React, { useState } from 'react';
 import './reset.css';
 import './App.css';
 
 function App() {
+  const [pText, setPtext] = useState('')
+
+  function handleWriting(event) {
+    const keyPressed = event.key
+    setPtext(pText + keyPressed)
+  }
+
   return (
     <div className="App">
       <i className='App__icon'></i>
-      <textarea className="App__Text" placeholder="Write anything..."></textarea>
+      <div onKeyDown={handleWriting} tabIndex={0} className="App__Text debug">
+        <p className="debug" >{pText}</p>
+      </div>
     </div>
   );
 }
